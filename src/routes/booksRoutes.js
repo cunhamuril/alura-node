901be6034bb88b40065abfbccbf26ca8e426f5e7
Murilo Cunha <murilo.sant@hotmail.com>
2022-06-24 13:04:05 -0300

    feat(books): craeted books search by publisher

--- src/routes/booksRoutes.js
+++ src/routes/booksRoutes.js
@@ -1,10 +1,12 @@
 import express from "express";
 
 import BooksController from "../controllers/booksController.js";
+import BooksByPublisherController from "../controllers/booksByPublisherController.js";
 
 const router = express.Router();
 
 router.get("/books", BooksController.index);
+router.get("/books/search", BooksByPublisherController.index);
 router.get("/books/:id", BooksController.show);
 router.post("/books", BooksController.store);
 router.put("/books/:id", BooksController.update);
