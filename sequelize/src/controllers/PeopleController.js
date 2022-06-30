9f17d4d4bb00595663e6aa864b15651152cec188
Murilo Cunha <murilo.sant@hotmail.com>
2022-06-30 17:44:36 -0300

    feat(sequelize): done people CRUD

--- sequelize/src/controllers/PeopleController.js
+++ sequelize/src/controllers/PeopleController.js
@@ -10,6 +10,75 @@ class PeopleController {
       return res.status(500).json(error.message);
     }
   }
+
+  static async show(req, res) {
+    const { id } = req.params;
+
+    try {
+      const person = await database.person.findOne({ where: { id } });
+
+      if (!person) {
+        return res.status(404).send("Person not found.");
+      }
+
+      return res.status(200).json(person);
+    } catch (error) {
+      return res.status(500).json(error.message);
+    }
+  }
+
+  static async store(req, res) {
+    const person = req.body;
+
+    try {
+      const createdPerson = await database.person.create(person);
+
+      return res.status(201).json(createdPerson);
+    } catch (error) {
+      return res.status(500).json(error.message);
+    }
+  }
+
+  static async update(req, res) {
+    const { id } = req.params;
+    const person = req.body;
+
+    try {
+      const requestedPerson = await database.person.findOne({ where: { id } });
+
+      if (!requestedPerson) {
+        return res.status(404).send("Person not found.");
+      }
+
+      await database.person.update(person, {
+        where: { id },
+      });
+
+      return res.status(204).send();
+    } catch (error) {
+      return res.status(500).json(error.message);
+    }
+  }
+
+  static async destroy(req, res) {
+    const { id } = req.params;
+
+    try {
+      const requestedPerson = await database.person.findOne({ where: { id } });
+
+      if (!requestedPerson) {
+        return res.status(404).send("Person not found.");
+      }
+
+      await database.person.destroy({
+        where: { id },
+      });
+
+      return res.status(204).send();
+    } catch (error) {
+      return res.status(500).json(error.message);
+    }
+  }
 }
 
 module.exports = PeopleController;
--- sequelize/src/routes/peopleRoutes.js
+++ sequelize/src/routes/peopleRoutes.js
@@ -5,5 +5,9 @@ const PeopleController = require("../controllers/PeopleController.js");
 const router = express.Router();
 
 router.get("/people", PeopleController.index);
+router.get("/people/:id", PeopleController.show);
+router.post("/people", PeopleController.store);
+router.put("/people/:id", PeopleController.update);
+router.delete("/people/:id", PeopleController.destroy);
 
 module.exports = router;
